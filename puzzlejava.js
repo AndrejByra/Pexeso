70dea50a861319713c0f30391199c12635300d0d
Andrej <andrej.byra@centrum.sk>
2017-12-03 16:21:47 +0100

    fixed bug

--- puzzlejava.js
+++ puzzlejava.js
@@ -4,6 +4,7 @@ var player=1;
 var field=[[1,1,2,2],[3,3,4,4],[5,5,6,6],[7,7,8,8]];
 console.log(field);
 shuffle();
+
 function shuffle()
 {
 	var i, temp;
@@ -15,26 +16,24 @@ var c2=Math.floor(Math.random()*4);
 temp=field[r1] [c1];
 field[r1] [c1]=field[r2] [c2];
 field[r2] [c2]=temp;
-
-
 } 
 console.log("shuffle");
 console.log(field);
 }
+
 function clickIMG(value)
 {
 	console.log(value);
 	var row=Math.floor(value/10);
 	var col=value%10;
 	console.log("your click row"+row+"coil"+col);
+
 	if(field[row][col]>0){
 		var source="p"+field[row][col]+".png";
 		var imageId="img"+row+col;
 		console.log(imageId);
 		document.getElementById(imageId).src=source;
-
 	}
-
 if (turn1==-1)
 		turn1=row*10+col;
 	else
@@ -43,6 +42,7 @@ if (turn1==-1)
 		checkSelectedCards();
 	}
 }
+
 function checkSelectedCards ()
 {
 	var row1=Math.floor(turn1/10);
@@ -55,11 +55,12 @@ function checkSelectedCards ()
 
 	}
 
-	else 
-		setTimeout(turnBack (), 3000);
+	else {
+		setTimeout(turnBack,2000);
+	}
 }
 
-function turnBack ()
+function turnBack()
 {
 	var row1=Math.floor(turn1/10);
 	var col1=turn1%10;
@@ -69,6 +70,6 @@ function turnBack ()
 	document.getElementById(imageID).src="back.png";
 	var imageID="img"+row2+col2;
 	document.getElementById(imageID).src="back.png";
-	turn1=1;
-	turn2=1;
+	turn1=-1;
+	turn2=-1;
 }
